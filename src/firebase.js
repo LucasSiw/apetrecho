7b663f46bb17f43547c3e5967a52ab0b73dd1d96
Lucas <lucaspaterno181@gmail.com>
2025-05-29 19:22:55 -0300

    Fazendo a comunicação com Firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyADwnOun2VO43PHC6fQSAGFdodph55qLZ8",
+  authDomain: "apetrecho-1d5f4.firebaseapp.com",
+  projectId: "apetrecho-1d5f4",
+  storageBucket: "apetrecho-1d5f4.firebasestorage.app",
+  messagingSenderId: "418846961668",
+  appId: "1:418846961668:web:a49b9c9f26ff1cb134c551"
+};
+
+const app = initializeApp(firebaseConfig);
